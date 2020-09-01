51dcd01e597802c2dd3d2eff898ad5dcbc30976b
James Thang <baokieuha@gmail.com>
2020-09-01 16:49:45 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import {Route} from "react-router-dom";
-import { BrowserRouter as Router } from 'react-router-dom'
 import HomePage from './Pages/HomePage/HomePage';
 import CostTrackingPage from './Pages/CostTrackingPage/CostTrackPage';
 import Footer from './components/Footer/footer.component';
@@ -17,15 +16,13 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Router>
-        <Route exact path='/' component={HomePage}  />
-        <Route exact path='/cost-tracking' component={CostTrackingPage}  />
-        <Route exact path='/contact' component={Contact} />
-        <Route exact path='/interest-cal' component={InterestPage} />
-        <Route exact path='/policy' component={PolicyList} />
-        <Route exact path='/policy/sudoku' component={PolicySudoku} />
-        <Route exact path='/sudoku' component={Sudoku} />
-      </Router>
+      <Route exact path='/' component={HomePage}  />
+      <Route exact path='/cost-tracking' component={CostTrackingPage}  />
+      <Route exact path='/contact' component={Contact} />
+      <Route exact path='/interest-cal' component={InterestPage} />
+      <Route exact path='/policy' component={PolicyList} />
+      <Route exact path='/policy/sudoku' component={PolicySudoku} />
+      <Route exact path='/sudoku' component={Sudoku} />
       <Footer />
     </div>
   );
