5a1ed9dedd97e56ff7cc2163f0ffabd225bf7a8c
James Thang <baokieuha@gmail.com>
2020-09-01 16:31:00 +0700

    react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {Route} from "react-router-dom";
-
+import { BrowserRouter as Router } from 'react-router-dom'
 import HomePage from './Pages/HomePage/HomePage';
 import CostTrackingPage from './Pages/CostTrackingPage/CostTrackPage';
 import Footer from './components/Footer/footer.component';
@@ -17,13 +17,15 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Route exact path='/' component={HomePage}  />
-      <Route exact path='/cost-tracking' component={CostTrackingPage}  />
-      <Route exact path='/contact' component={Contact} />
-      <Route exact path='/interest-cal' component={InterestPage} />
-      <Route exact path='/policy' component={PolicyList} />
-      <Route exact path='/policy/sudoku' component={PolicySudoku} />
-      <Route exact path='/sudoku' component={Sudoku} />
+      <Router>
+        <Route exact path='/' component={HomePage}  />
+        <Route exact path='/cost-tracking' component={CostTrackingPage}  />
+        <Route exact path='/contact' component={Contact} />
+        <Route exact path='/interest-cal' component={InterestPage} />
+        <Route exact path='/policy' component={PolicyList} />
+        <Route exact path='/policy/sudoku' component={PolicySudoku} />
+        <Route exact path='/sudoku' component={Sudoku} />
+      </Router>
       <Footer />
     </div>
   );
