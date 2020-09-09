ef564a25190f8c2c4e5e6deb2e120c6321eaa138
James Thang <baokieuha@gmail.com>
2020-09-09 11:41:30 +0700

    adding marketing for finance advisor

--- src/Pages/AppIntroduction/Finance/FinanceAdvisor.jsx
+++ src/Pages/AppIntroduction/Finance/FinanceAdvisor.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import logo from '../../../Image/financeLogo.png';
+import './FinanceAdvisor.scss';
+import appstore from '../../../Image/appStore.png';
+import screenshot1 from '../../../Image/finance1_iphonexspacegrey_portrait.png';
+import screenshot2 from '../../../Image/finance2.png';
+import screenshot3 from '../../../Image/finance3_iphonexspacegrey_portrait.png';
+import screenshot6 from '../../../Image/finance6_iphonexspacegrey_portrait.png';
+import screenshot8 from '../../../Image/finance8_iphonexspacegrey_portrait.png';
+
 
 const FinanceAdvisor = () => {
     return (
-        <div>
-            
+        <div className="container">
+            <h2 className="title_fi">More than a Calculator</h2>
+            <img className="screenshot_fi" alt="screen shot" src={screenshot3} />
+            <h3 className="reveal">3 Fundamental Valuation Models</h3>
+            <h3>Easily computing Present Value and Future Value</h3>
+            <img className="screenshot_fi" alt="screen shot" src={screenshot6} />
+            <h3>Detail Loan Table</h3>
+            <img className="screenshot_fi" alt="screen shot" src={screenshot8} />
+            <img className="screenshot_fi" alt="screen shot" src={screenshot1} />
+
+            <img className="screenshot_fi" alt="screen shot" src={screenshot2} />
+
+            <h3>Learn more knowledge on Finance Field</h3>
+            <h3>Advailable on Iphone and Ipad</h3>
+            <h3 className="seperation">Finance Advisor</h3>
+
+            <div className="end-container">
+                <img className="appstore_fi" alt="AppStore" src={appstore}/>
+                <img className="applogo" alt="logo" src={logo} />
+            </div>
+          
         </div>
     )
 }
