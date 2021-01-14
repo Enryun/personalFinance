0ab45754a80bc5fca0ceb731bafb055299fe93c9
James Thang <baokieuha@gmail.com>
2021-01-14 17:23:17 +0700

    working on POS intro Pages

--- src/Pages/AppIntroduction/POS/Pos.jsx
+++ src/Pages/AppIntroduction/POS/Pos.jsx
@@ -6,6 +6,7 @@ import screenshot4 from '../../../Image/POS4.webp';
 import './POS.scss'
 import appstore from '../../../Image/appStore.png';
 import {Link} from 'react-router-dom';
+import logo from '../../../Image/POS_Logo.png';
 
 
 
@@ -13,13 +14,21 @@ const Pos = () => {
     return (
         <div>
             <div className="big-background">
-                <h1 className="big-title">Transform your Ipad</h1>
+                <h1>Transform your Ipad with</h1>
+                <div className="pos-container">
+                    <h1 className="pos">UCoffee</h1>
+                    <a href="https://apps.apple.com/us/app/id1530719536?fbclid=IwAR2I0YNm3eYD8i6E-y2pQWpGnGtfaMNTA5EjsqgM5REU3MRlyN-Lq29nKQY" target="_blank" rel="noopener noreferrer">
+                        <img className="applogo" alt="AppStore" src={logo}/>
+                    </a>
+                </div>
+                
+                
                 <img src={screenshot1} alt="" className="image_1" />
 
             </div>
             
             <div className="pos_block">
-                <h2 className="small-title">Easy to Use and <Link className="link" to='/ucoffee-setup'>Set Up</Link></h2>
+                <h2>Easy to Use and <Link className="link" to='/ucoffee-setup'>Set Up</Link></h2>
                 <div class="gradient-border" id="box">
                     <img src={screenshot3} alt="" className="other_image" />
 
@@ -44,7 +53,7 @@ const Pos = () => {
                 <a href="https://apps.apple.com/app/id1529449644?fbclid=IwAR2I0YNm3eYD8i6E-y2pQWpGnGtfaMNTA5EjsqgM5REU3MRlyN-Lq29nKQY" target="_blank" rel="noopener noreferrer">
                     <img className="appstore" alt="AppStore" src={appstore}/>
                 </a>
-                <h3>Already Download. Go to <Link to='/ucoffee-setup' className="link">Set Up</Link></h3>
+                <h3>Already Downloaded. Go to <Link to='/ucoffee-setup' className="link">Set Up</Link></h3>
             </div>
             
         </div>
--- src/Pages/AppIntroduction/POS/PosSetUp.jsx
+++ src/Pages/AppIntroduction/POS/PosSetUp.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import './POS.scss'
+import screenshot1 from "../../../Image/POS5.jpg"
+import screenshot2 from "../../../Image/POS6.jpg"
+import screenshot3 from "../../../Image/POS7.jpg"
+import screenshot4 from "../../../Image/POS8.jpg"
+import screenshot5 from "../../../Image/POS9.jpg"
+import screenshot6 from "../../../Image/POS10.webp"
+import screenshot7 from "../../../Image/POS11.jpg"
+
 
 const PosSetUp = () => {
     return (
         <div>
-            <h1>Hello from Set up</h1>
+
+            <h1 className="pos">UCoffee</h1>
+            <h2>1. Register New User</h2>
+            <img src={screenshot6} alt="" className="other_image" />
+            <img src={screenshot5} alt="" className="other_image" />
+
+            <h2>2. Create New Item in Menu</h2>
+            <img src={screenshot7} alt="" className="other_image" />
+            <img src={screenshot1} alt="" className="other_image" />
+            <img src={screenshot2} alt="" className="other_image" />
+            <img src={screenshot3} alt="" className="other_image" />
+
+            <h2>3. Make Order</h2>
+            <img src={screenshot4} alt="" className="other_image" />
+
+            <h2>4. Additional Information of your Shop</h2>
+
+            <h2>5. Connect to POS Printer</h2>
+            <h2>6. View Purchase History</h2>
+            <h2>7. Double Check money at the end of the Day</h2>
+            <h2>Remotely Control from other Devices</h2>
         </div>
+
+
     )
 }
 
